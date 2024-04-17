9931080ab82f02783e6812d496993ae687d1191b
Juan Cadena <juanm.cadena@udea.edu.co>
2024-04-16 22:40:44 -0500

    Adding video-views structures

---
+++ src/main/java/culturemedia/service/CulturemediaImplementation.java
@@ -0,0 +1,24 @@
+package culturemedia.service;
+
+import culturemedia.model.Video;
+import culturemedia.model.View;
+
+import java.util.List;
+
+public class CulturemediaImplementation implements  CulturemediaService{
+
+    @Override
+    public List<Video> findAllVideo() {
+        return null;
+    }
+
+    @Override
+    public Video add(Video video) {
+        return null;
+    }
+
+    @Override
+    public View add(View view) {
+        return null;
+    }
+}
