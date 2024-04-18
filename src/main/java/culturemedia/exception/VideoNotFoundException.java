dd2729403a998918fb5c1183ee001d1db76256c5
Juan Cadena <juanm.cadena@udea.edu.co>
2024-04-17 20:53:00 -0500

    Corrections

--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -2,10 +2,10 @@ package culturemedia.exception;
 
 import java.text.MessageFormat;
 
-public class VideoNotFoundException extends Exception{
+public class VideoNotFoundException extends CulturemediaException{
     public VideoNotFoundException(){
         super(("Video not Found"));
-    }
+    }//TODO Actualizar mensaje
     public VideoNotFoundException(String title){
         super(MessageFormat.format("Video not found with this tile: {0}", title));
     }
